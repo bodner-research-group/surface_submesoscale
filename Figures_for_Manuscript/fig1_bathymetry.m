4265bfdec185e2f20dc5abc4c1bcb1395ce577d4
siyidongfang <y_si@mit.edu>
2026-01-01 19:08:21 -0500

    update: figures

---
+++ Figures_for_Manuscript/fig1_bathymetry.m
@@ -0,0 +1,7 @@
+%%%%% Figure 1:
+%%%%% Panel 1: bathymetry
+
+
+
+%%%%% Panels 4-7: winter zeta/f, sigma/|f|, Delta/f
+%%%%% Panels 9-10: summer zeta/f, sigma/|f|, Delta/f
---
+++ Figures_for_Manuscript/fig2.m
@@ -0,0 +1,10 @@
+
+
+%%%%% 9-Panels: 
+%%%%% Row 1 [m/m]: SSH, submesoscale SSH, and steric height
+
+%%%%% Row 2 [m/s]: g/f * gradient magnitude of SSH, submesoscale SSH, and steric height
+
+%%%%% Row 3 [non-dimensional ~ Rossby number]: g/f^2 * Laplacian of SSH, submesoscale SSH, and steric height
+
+
