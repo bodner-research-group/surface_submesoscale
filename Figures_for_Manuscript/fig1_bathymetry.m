909b16b9d15004b35f81f7d644edb98739c0e5be
siyidongfang <y_si@mit.edu>
2026-01-18 15:33:27 -0500

    hourly wb_eddy using Gaussian filter

--- Figures_for_Manuscript/fig1_bathymetry.m
+++ Figures_for_Manuscript/fig1_bathymetry.m
@@ -12,8 +12,8 @@ lon = ncread(ncfile,'lon');
 topo = ncread(ncfile,'z');
 
 %%% ===== Crop to your region =====
-latlim = [49.99 70.01];
-lonlim = [-41.68 0.01];
+latlim = [49.99+3 70.01-3];
+lonlim = [-41.68+5 0.01-7];
 
 lat_idx = find(lat >= latlim(1) & lat <= latlim(2));
 lon_idx = find(lon >= lonlim(1) & lon <= lonlim(2));
@@ -26,11 +26,14 @@ topo_sub = topo(lon_idx, lat_idx);
 [Lon, Lat] = meshgrid(lon_sub, lat_sub);
 
 %%% ===== Colormap =====
-mycolor1 = flip(cmocean('diff'));
-mycolor1 = mycolor1(1:2:128,:);
+% mycolor1 = flip(cmocean('diff'));
+% mycolor1 = mycolor1(1:2:128,:);
+% mycolor2 = flip(cmocean('ice'));
+% mycolor2 = mycolor2(1:2:end,:);
+% mycolor  = [mycolor1; mycolor2];
+
 mycolor2 = flip(cmocean('ice'));
-mycolor2 = mycolor2(1:2:end,:);
-mycolor  = [mycolor1; mycolor2];
+mycolor  = [boxcolor; mycolor2];
 
 fontsize = 17;
 
@@ -38,35 +41,64 @@ fontsize = 17;
 lat0 = mean(lat_sub);
 lon0 = mean(lon_sub);
 
+
+%%
 figure(1); clf; set(gcf,'Color','w');
 
 axesm('lambert', ...
       'MapLatLimit', latlim, ...
       'MapLonLimit', lonlim, ...
-      'MapParallels', [55 65], ...
+      'MapParallels', [55 60], ...  
       'Frame', 'on', ...
       'Grid', 'on', ...
       'MeridianLabel','on', ...
       'ParallelLabel','on');
 
-axis off; hold on;
+% Adjust grid line spacing manually
+setm(gca, 'Grid', 'on'); % Enable grid lines
+
+% Set the spacing for grid lines (in degrees)
+setm(gca, 'MLineLocation', 4);  % Meridian gridlines every 1 degree
+setm(gca, 'PLineLocation', 2);  % Parallel gridlines every 1 degree
+
+% 
+% % Adjust appearance of the gridlines
+% setm(gca, 'GridColor', 'k');  % Change grid color to black
+% setm(gca, 'GridLineStyle', '--');  % Dashed gridlines
 
 %%% ===== Plot bathymetry =====
 pcolorm(Lat, Lon, -topo_sub'/1000);
 shading flat;
 
+% axis off; 
+hold on;
+
 %%% ===== Plot zero contour =====
 black = [0 0 0];
 contourm(Lat, Lon, -topo_sub'/1000, [0 0], 'EdgeColor', black, 'LineWidth', 1.2);
+% contourm(Lat, Lon, -topo_sub'/1000, [1 1], '-','EdgeColor', black, 'LineWidth', 0.5);
+% contourm(Lat, Lon, -topo_sub'/1000, [2 2], '-','EdgeColor', black, 'LineWidth', 0.5);
+% contourm(Lat, Lon, -topo_sub'/1000, [3 3], '-','EdgeColor', black, 'LineWidth', 0.5);
+% contourm(Lat, Lon, -topo_sub'/1000, [4 4], '-','EdgeColor', black, 'LineWidth', 0.5);
+
+%%% ===== Set grid lines interval using m_grid =====
+% Initialize the map projection (if necessary)
+% m_proj('lambert', 'lat', latlim, 'lon', lonlim); 
+
+% Use m_grid to control grid intervals manually
+% m_grid('parallel', 2, 'meridian', 5, 'LineStyle', '--', 'GridColor', 'k', 'LineWidth', 0.5);
+
 
 %%% ===== Plot box =====
 lon_box = [-27  -17.3  -17.3  -27  -27];
 lat_box = [ 58    58     62     62    58];
 plotm(lat_box, lon_box, 'k--', 'LineWidth', 2);
 
+
 %%% ===== Colormap & colorbar =====
 colormap(mycolor);
-clim([-length(mycolor1)/length(mycolor2) 1]*5);
+% clim([-length(mycolor1)/length(mycolor2) 1]*5);
+clim([0 5]);
 
 cb = colorbar;
 cb.Label.String = 'Depth (km)';
@@ -74,3 +106,4 @@ cb.Ticks = 0:1:5;
 set(cb,'YDir','reverse');
 
 set(gca,'fontsize',fontsize);
+box off
